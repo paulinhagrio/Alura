c592fc02a364111b9aa299db6922242db3daf2d5
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-12-06 16:58:01 -0300

    Spring Boot API REST construa uma API

---
+++ backend JAVA/Persistência com JPA Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,58 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CadastroDeProduto {
+    public static void main(String[] args) {
+        cadastrarProdutos();
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscaPorId(1l);
+        System.out.println(p.getPreco());
+
+//        List<Produto> todos = produtoDao.buscarTodos();
+//        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+//        List<Produto> todos = produtoDao.buscarPorNome("Xiaomi Redmi");
+//        todos.forEach(p3 -> System.out.println(p.getNome()));
+
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p3 -> System.out.println(p.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preço do produto: " + precoDoProduto);
+
+    }
+
+    private static void cadastrarProdutos() {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800") , celulares );
+
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+
+}
---
+++ backend JAVA/Spring Boot API REST construa uma API/forum/src/main/java/br/com/alura/forum/modelo/StatusTopico.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.modelo;
+
+import javax.persistence.Entity;
+
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}
